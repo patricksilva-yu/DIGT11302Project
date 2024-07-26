b94fbe9b6f52cd3fd203d480c90c79be0ac39d86
Toby Carter <148788610+Toby-J-Carter@users.noreply.github.com>
2024-07-26 07:58:21 -0400

    Profile page changes

---
+++ js/profile.js
@@ -0,0 +1,49 @@
+window.addEventListener("DOMContentLoaded", DOMLoaded);
+
+function DOMLoaded() {
+    const submitButton = document.getElementById("submit");
+    submitButton.addEventListener("click", login);
+}
+
+function login(event) {
+    event.preventDefault();
+    let isValid = true;
+    const form = document.querySelector('form');
+    const username = document.getElementById("username");
+    const password = document.getElementById("password");
+    const name  = document.getElementById("name");
+    const fieldsets = document.querySelectorAll("fieldset");
+
+    const errorMessages = form.querySelectorAll('.error-message');
+    errorMessages.forEach(error => error.remove());
+    password.style.borderColor = 'green';
+    
+    const passwordPattern = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
+    if (!passwordPattern.test(password.value)) {
+        isValid = false;
+        showError(password, 'Incorrect password.');
+    }
+
+    name.innerHTML = username.value;
+
+    if (isValid) {
+    fieldsets[0].hidden = "true";
+    fieldsets[1].removeAttribute("hidden");
+    fieldsets[2].removeAttribute("hidden");
+    fieldsets[3].removeAttribute("hidden");
+    }
+}
+
+function showError(element, message) {
+    const error = document.createElement('div');
+    error.className = 'error-message';
+    error.textContent = message;
+
+    if (element.nextSibling) {
+        element.parentNode.insertBefore(error, element.nextSibling);
+    } else {
+        element.parentNode.appendChild(error);
+    }
+
+    element.style.borderColor = 'red';
+}
